b052010e0fe6cf2d1b64defa596eb76d8bd7fcdc
chenuvi <chenuvi@outlook.com>
2023-09-02 22:19:52 +0800

    请求参数

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	// 2. 请求参数
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+        url := r.URL
+		query := url.Query()
 
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("wwwroot")))
+		id := query["id"]
+		log.Println(id)
+		name := query.Get("name")
+		log.Println(name)
+    })
+
+	// 1. 文件服务器
+
+	// http.ListenAndServe(":8080", http.FileServer(http.Dir("wwwroot")))
 
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	http.ServeFile(w, r, "wwwroot" + r.URL.Path) 
 	// })
-	// http.ListenAndServe(":8080", nil)
+
+
+	http.ListenAndServe(":8080", nil)
 }
